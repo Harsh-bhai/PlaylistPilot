dce5633aaefc555cb3aaacf1c99e59f9f254ee42
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-03-10 12:44:26 +0530

    Update options.js

--- pages/options.js
+++ pages/options.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import axios from 'axios'
+import Cookies from 'js-cookie'
 
 const Options = () => {
   return (
-    <div className='min-h-screen'>Options</div>
+    <div className='min-h-screen'>
+      Options
+      </div>
   )
 }
 
 export default Options
+
+
+export async function getServerSideProps(context) {
+  let res=await axios.get("https://api.spotify.com/v1/search",{
+    headers:{Authorization:`Bearer ${Cookies.get('token')}`},
+    // params:{q:}
+  })
+  let data = res.data
+  return {
+    props: {data},
+  }
+}
