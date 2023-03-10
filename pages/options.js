c4d09056ebbf8a51ef53f22323dc578b4358a16d
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-03-10 13:11:05 +0530

    updated option page

--- pages/options.js
+++ pages/options.js
@@ -1,11 +1,65 @@
 import React from 'react'
-import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const Options = () => {
   return (
-    <div className='min-h-screen'>
-      Options
+    <div className='min-h-screen text-white flex flex-col space-y-4 items-center'>
+      <h1 className='text-5xl font-bold'>Options</h1>
+      <h2>Create, Modify your Playlist here ...</h2>
+      <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+        </a>
+       
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261"/>
+        </a>
+       
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262"/>
+        </a>
+        
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263"/>
+        </a>
+      
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264"/>
+        </a>
+       
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265"/>
+        </a>
+      
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267"/>
+        </a>
+        
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268"/>
+        </a>
+        
+      </div>
+    </div>
+  </div>
+</section>
       </div>
   )
 }
@@ -14,12 +68,18 @@ export default Options
 
 
 export async function getServerSideProps(context) {
-  let res=await axios.get("https://api.spotify.com/v1/search",{
-    headers:{Authorization:`Bearer ${Cookies.get('token')}`},
+  const token = context.req.cookies['token']    
+  // console.log(token)
+ 
+  let res=await fetch("https://api.spotify.com/v1/search?query=Atif Aslam&type=artist",{
+    headers:{Authorization:`Bearer ${token}`},
     // params:{q:}
   })
-  let data = res.data
+
+  let data = await res.json()
+  console.log(data)
   return {
     props: {data},
   }
+ 
 }
